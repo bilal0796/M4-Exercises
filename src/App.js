ca9f39ad08668ca677a3315aabc1bbd32659ae1c
Bilal Ahmad <bilal.ahmad0796@gmail.com>
2021-11-09 16:59:39 +0100

    added jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNavbar from './Components/MyNavbar';
+import WelcomePage from './Components/WelcomePage';
+import MyFooter from './Components/MyFooter';
 function App() {
   return (
     <div>
       <MyNavbar />
+      <WelcomePage />
+      <MyFooter />
     </div>
   )
 }
---
+++ src/Components/MyFooter.jsx
@@ -0,0 +1,9 @@
+import { footer } from 'react-bootstrap'
+const MyFooter = () => (
+    <footer class="footer">
+        <div class="container">
+            <span class="text-muted">Place sticky footer content here.</span>
+        </div>
+    </footer>
+)
+export default MyFooter
